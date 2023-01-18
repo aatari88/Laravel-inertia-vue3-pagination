3d71a07652354d728b83020f09bfcdeae7e24b58
Aaron Tapia <atapiar88@gmail.com>
2023-01-18 07:12:51 -0500

    first commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,12 +9,8 @@ class PostController extends Controller
 {
     public function index(Request $request){
 
-        // if (!$request->has('page')) {
-        //     return inertia()
-        // }
-
         $posts = Post::paginate(4);
-        // return inertia('Posts/Index', compact('posts'));
+
         return inertia('Posts/Index', ['posts' => $posts]);
     }
 }
